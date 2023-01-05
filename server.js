9c732c2ac672541ab11ac04e13be3935a25f5b84
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-05 13:48:54 -0600

    add cors and dotenv dependencies as well as nodemon dev dependency

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
+const dotenv = require('dotenv').config({path: __dirname+'/config/.env'})
+
 const express = require('express');
 const app = express()
+const cors = require('cors')
+
+const PORT = process.env.PORT
+
+app.use(cors())
+
+app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`)
+})
