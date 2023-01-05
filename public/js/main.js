f24eec3f1160ce3375a7f051f7f0c4039687925c
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-05 15:06:43 -0600

    add the compareChoices function which calls the server and displays the result of rockPaperScissors on index.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-//Example fetch using pokemonapi.co
+// Example fetch using pokemonapi.co
 // document.querySelector('button').addEventListener('click', getFetch)
 
 // function getFetch(){
@@ -14,6 +14,10 @@
 //           console.log(`error ${err}`)
 //       });
 // }
+
+
+// grabs the <p> element where result of each hand will be displayed
+const handResultElement = document.querySelector('.hand-result')
 
 // "Click to start prompt - Asking User to make a selection"
 const start = document.getElementById('startBtn').addEventListener('click', startGame);
@@ -37,6 +41,24 @@ function rockPaperScissors(human, computer) {
     return "Computer Wins!";
   }
 }
+
+// compareChoices function will compare the human and computer choices;
+// it does so by passing the arguments humanChoice and computerChoice into the rockPaperScissors function.
+// then, the result from rockPaperScissors is stored in a variable labeled result.
+// result is then displayed in the appropriate element
+const compareChoices = async () => {
+  // computerChoice is handled by the server which returns 'rock', 'paper', or 'scissors'
+  let computerChoice = await fetch('/computerChoice').then(res => res.json())
+  let handResult = rockPaperScissors(humanChoice, computerChoice)
+  handResultElement.innerText = handResult
+}
+
+// we need a way to get the user selection
+let humanChoice = 'scissors'
+
+// we would want to run this compareChoices when a user clicks on the rock, paper, or scissors button
+// for now, we are calling it directly (i.e., everytime the page refreshes) to show functionality and where the results will be displayed
+compareChoices()
 
 // console.log(randomRpsGenerator());
 // console.log(rockPaperScissors("scissors"));
