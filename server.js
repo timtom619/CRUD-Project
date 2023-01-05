f2e954e6fdf29b3efbba52c34ea198f774c10dbb
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-05 13:56:22 -0600

    serve index.html

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT
 app.use(cors())
 app.use(express.static('public'))
 
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html')
+})
 
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`)
