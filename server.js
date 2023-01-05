78c5e804cadfacaed7002008d2833c6a356f6a52
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-05 13:52:55 -0600

    enable app to utilize a public directory

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cors = require('cors')
 const PORT = process.env.PORT
 
 app.use(cors())
+app.use(express.static('public'))
+
+
 
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`)
